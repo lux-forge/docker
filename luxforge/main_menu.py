22a31ad1bf20c7c026fad4a305a36d96d60448f2
luxforge <lab@luxforge.dev>
2025-10-06 01:57:33 +0100

    Made it interactive!!

--- luxforge/main_menu.py
+++ luxforge/main_menu.py
@@ -15,13 +15,27 @@ class MainMenu(Menu):
     options = {
         "D": ("Docker", "load_docker_menu"),
         "P": ("Load Paths", "load_paths_menu"),
+        "U": ("User Management", "load_user_menu"),
+        "G": ("Games", "load_games_menu"),
     }
     menu_name = "Main Menu"
     
     def load_docker_menu(self):
         from docker.menu import DockerMenu
         DockerMenu(previous_menu=self).launch()
-        
+    
+    def load_paths_menu(self):
+        from utils.pathloader import paths
+        paths.print_paths()
+        input("Press Enter to return to the menu...")
+    
+    def load_user_menu(self):
+        from users.users import UserMenu
+        UserMenu(previous_menu=self).launch()
+    
+    def load_games_menu(self):
+        from games.menu import GamesMenu
+        GamesMenu(previous_menu=self).launch()
 
 if __name__ == "__main__":
     menu = MainMenu()
